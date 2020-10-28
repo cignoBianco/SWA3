7e4d3f72890288aca4d7312157338c8695597039
cignobianco <smiana123@gmail.com>
2020-10-28 17:09:37 +0700

    contacts and team

--- src/components/statics/Contacts.js
+++ src/components/statics/Contacts.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './../layout.css'
+import img1 from './../../assets/svg/customer_service.svg'
 import { Layout, Tag, Row, Col, Statistic  } from 'antd'
 import { 
     TwitterOutlined,
@@ -12,17 +13,10 @@ import {
 const Contacts = () => {
 
   return (
-    <div >
-        <div>
-            <Row gutter={16}>
-                <Col span={12}>
-                <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
-                </Col>
-                <Col span={12}>
-                <Statistic title="Unmerged" value={93} suffix="/ 100" />
-                </Col>
-            </Row>
-        </div>
+    <Row className="contact-row">
+      <Col span={12}>
+          <h1>Contacts our Support<br/>and Development team</h1>
+          <p>Need to get in touch with the team? We're all ears.</p>
         <div>
             <div>
                 <Tag icon={<TwitterOutlined />} color="#55acee">
@@ -39,7 +33,11 @@ const Contacts = () => {
                 </Tag>
             </div>
         </div>
-    </div>
+      </Col>
+      <Col span={12}>
+        <img src={img1} />
+      </Col>
+    </Row>
   );
 };
 
--- src/components/statics/Team.js
+++ src/components/statics/Team.js
@@ -1,22 +1,98 @@
+import React, { useState } from 'react';
+import './../layout.css'
+import htmlImg from './../../assets/svg/html.svg'
+import cssImg from './../../assets/svg/css.svg'
+import jsImg from './../../assets/svg/javascript.svg'
+import uiImg from './../../assets/svg/ui_designer.svg'
+import coderImg from './../../assets/svg/coder.svg'
+import supportImg from './../../assets/svg/it_support.svg'
+//import  from './../../assets/svg/'
+import { Tag, Row, Col, Statistic, Card } from 'antd'
 import { 
     TwitterOutlined,
     YoutubeOutlined,
     FacebookOutlined,
     LinkedinOutlined,
+    LikeOutlined
 } from '@ant-design/icons';
-import React, { useState } from 'react';
-import './../layout.css'
-import { Layout, Tag } from 'antd'
-const { Header: H, Footer, Content } = Layout;
+const { Meta } = Card
 
-const Team = () => {
+const Contacts = () => {
 
   return (
-    <div >
+    <div class="contact-wrapper">
         <div>
-
+        <h1 style={{marginTop: 30}}>Our Team Leads</h1>
+        <p>We are led by a team who constantly questions, tinkers, and<br/> <code>challendgers</code> to unlock great creativity eround every turn.</p>
+        <Row gutter={16} style={{textAlign: '-webkit-center', marginTop: 30}}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={htmlImg} className="imgTeam"  />}
+                >
+                    <Meta title="Smirnova Anastasia" description="LAYOUT HTML" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={cssImg} className="imgTeam" />}
+                >
+                    <Meta title="Smirnova Anastasia" description="LAYOUT DESIGNER" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={jsImg} className="imgTeam" />}
+                >
+                    <Meta title="Smirnova Anastasia" description="FRONTEND DEVELOPER" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={coderImg} className="imgTeam"  />}
+                >
+                    <Meta title="Smirnova Anastasia" description="BACKEND DEVELOPER" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={uiImg} className="imgTeam"  />}
+                >
+                    <Meta title="Smirnova Anastasia" description="UI DESIGNER" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src={supportImg} className="imgTeam"  />}
+                >
+                    <Meta title="Smirnova Anastasia" description="SUPPORT" />
+                </Card>
+            </Col>
+        </Row>
         </div>
         <div>
+        <div>
+            <Row gutter={16}>
+                <Col span={12}>
+                <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
+                </Col>
+                <Col span={12}>
+                <Statistic title="Unmerged" value={93} suffix="/ 100" />
+                </Col>
+            </Row>
+        </div>
+        <div style={{marginTop: 20}}>
             <div>
                 <Tag icon={<TwitterOutlined />} color="#55acee">
                     Twitter
@@ -32,10 +108,11 @@ const Team = () => {
                 </Tag>
             </div>
         </div>
+        </div>
     </div>
   );
 };
 
-export default Team;
+export default Contacts;
 
 
